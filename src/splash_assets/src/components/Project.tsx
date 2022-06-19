5d18f3b2f8a9b529a43211d03011faf631321ea1
Nikhil Gupta <nikhilgupta.iitk@gmail.com>
2022-06-19 12:11:40 +0530

    adds ability to modify positions

--- src/splash_assets/src/components/Project.tsx
+++ src/splash_assets/src/components/Project.tsx
@@ -1,5 +1,5 @@
-import { Circle, CircleOutlined, FormatShapes, Rectangle, RectangleOutlined } from "@mui/icons-material";
-import { Divider, IconButton, Stack } from "@mui/material";
+import { Circle, CircleOutlined, ExpandMore, FormatShapes, Rectangle, RectangleOutlined, Spa } from "@mui/icons-material";
+import { Accordion, AccordionDetails, AccordionSummary, Card, CardContent, Divider, IconButton, Stack, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { Path } from "canvaskit-wasm";
 import React, { useEffect, useState } from "react"
@@ -7,6 +7,11 @@ import { Shape, useSplashContext } from "../store/SplashContext"
 
 export const Project = () => {
     const { canvasKit, shapes, setShapes } = useSplashContext();
+
+    const [x, setX] = useState<number>(20);
+    const [y, setY] = useState<number>(20);
+    const [width, setWidth] = useState<number>(100);
+    const [height, setHeight] = useState<number>(100);
 
     const loadProject = () => {
         if (!canvasKit) { return }
@@ -44,14 +49,14 @@ export const Project = () => {
     const addRectangle = () => {
         setShapes(shapes.concat(new Shape(
             0, 
-            [20, 20, 100, 100]
+            [x, y, width, height]
         )))
     }
 
     const addCircle = () => {
         setShapes(shapes.concat(new Shape(
             1, 
-            [20, 20, 100, 100]
+            [x, y, width, height]
         )))
     }
 
@@ -73,8 +78,53 @@ export const Project = () => {
                 {/* <Box style={{ flex: 0.25 }} /> */}
             </Stack>
             <Divider orientation="vertical" />
-            <Stack width={250}>
-                <div>Properties</div>
+            <Stack width={250} alignItems="center">
+                <Accordion>
+                    <AccordionSummary
+                    expandIcon={<ExpandMore />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                    <Typography>Project Settings</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                        You will be able to edit project settigns here.
+                    </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                    <AccordionSummary
+                    expandIcon={<ExpandMore />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                    <Typography>Artboard Settings</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                        You will be able to edit artboard settigns here.
+                    </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion style={{ width: "100%" }}>
+                    <AccordionSummary
+                    expandIcon={<ExpandMore />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                    <Typography>Position</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <TextField label="X" type="number" defaultValue={x} onChange={(e) => setX(parseInt(e.currentTarget.value))}/>
+                    <Box height={10}/>
+                    <TextField label="Y" type="number" defaultValue={y} onChange={(e) => setY(parseInt(e.currentTarget.value))}/>
+                    <Box height={10}/>
+                    <TextField label="Width" type="number" defaultValue={width} onChange={(e) => setWidth(parseInt(e.currentTarget.value))}/>
+                    <Box height={10}/>
+                    <TextField label="Height" type="number" defaultValue={height} onChange={(e) => setHeight(parseInt(e.currentTarget.value))}/>
+                    </AccordionDetails>
+                </Accordion>
             </Stack>
             <Divider orientation="vertical" />
         </Stack>
